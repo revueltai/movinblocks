63a02aadfca2279d4bfb772a1d0ecf8cb6195589
“Ignacio” <revuelta.ig@gmail.com>
2024-12-29 16:24:39 +0100

    chore: bundle plugin

--- dist/movinblocks.esm.js
+++ dist/movinblocks.esm.js
@@ -34,7 +34,9 @@ class Movinblocks {
     __publicField(this, "_overlap", 0);
     __publicField(this, "_options", {});
     __publicField(this, "_events", {});
-    __publicField(this, "_cssPrefix", "--mb-");
+    __publicField(this, "_cssVarPrefix", "--mb-");
+    __publicField(this, "_cssBaseClass", "mb");
+    __publicField(this, "_cssVisibleClass", "mb-visible");
   }
   _validateTimeline() {
     if (!this._options.timeline) {
@@ -45,6 +47,21 @@ class Movinblocks {
       if (!el) {
         throw new Error(`Element id "${id}" does not exist.`);
       }
+    }
+    return true;
+  }
+  _validateArrayProp(prop) {
+    const mbOption = this._options[prop];
+    const mbOptionLength = mbOption.length;
+    const timelineLength = this._options.timeline.length;
+    if (prop === "overlap") {
+      if (mbOptionLength !== timelineLength - 1) {
+        throw new Error(`The "${prop}" array must be one element shorter than the timeline. ${timelineLength - 1} elements expected, got ${mbOptionLength} instead.`);
+      }
+      return true;
+    }
+    if (mbOptionLength !== timelineLength) {
+      throw new Error(`The "${prop}" array must be the same length as timeline. ${timelineLength} elements expected, got ${mbOptionLength} instead.`);
     }
     return true;
   }
@@ -84,6 +101,7 @@ class Movinblocks {
       return this._options.duration;
     }
     if (Utils.isArray(this._options.duration)) {
+      this._validateArrayProp("duration");
       return this._options.duration[index];
     }
     return this._duration;
@@ -93,6 +111,7 @@ class Movinblocks {
       return this._options.animation;
     }
     if (Utils.isArray(this._options.animation)) {
+      this._validateArrayProp("animation");
       return this._options.animation[index];
     }
     return this._animation;
@@ -102,6 +121,7 @@ class Movinblocks {
       return this._options.timingFunction;
     }
     if (Utils.isArray(this._options.timingFunction)) {
+      this._validateArrayProp("timingFunction");
       return this._options.timingFunction[index];
     }
     return this._timingFunction;
@@ -112,6 +132,7 @@ class Movinblocks {
         return this._options.overlap;
       }
       if (Utils.isArray(this._options.overlap)) {
+        this._validateArrayProp("overlap");
         return this._options.overlap[index - 1];
       }
     }
@@ -121,19 +142,40 @@ class Movinblocks {
     let currDelay = 0;
     let prevDuration = 0;
     for (const item of this._payload) {
-      item.el.classList.add("mb");
+      item.el.classList.add(this._cssBaseClass);
       item.el.classList.add(item.animation);
       if (prevDuration && item.overlap) {
         currDelay += prevDuration - item.overlap;
       }
-      Utils.setCssVar(item.el, `${this._cssPrefix}duration`, `${item.duration}ms`);
-      Utils.setCssVar(item.el, `${this._cssPrefix}delay`, `${currDelay}ms`);
-      Utils.setCssVar(item.el, `${this._cssPrefix}timing-function`, item.timingFunction);
+      Utils.setCssVar(item.el, `${this._cssVarPrefix}duration`, `${item.duration}ms`);
+      Utils.setCssVar(item.el, `${this._cssVarPrefix}timing-function`, item.timingFunction);
+      if (this._options.viewportTrigger) {
+        this._addObserver(item.el);
+      } else {
+        Utils.setCssVar(item.el, `${this._cssVarPrefix}delay`, `${currDelay}ms`);
+        item.el.classList.add(this._cssVisibleClass);
+      }
       item.el.addEventListener("animationstart", () => this._handleAnimationStart());
       item.el.addEventListener("animationend", () => this._handleAnimationEnd(item.id));
       item.el.addEventListener("animationiteration", () => this._handleAnimationIteration());
       prevDuration = item.duration;
     }
+  }
+  _addObserver(el) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const index = this._options.timeline.indexOf(entry.target.id);
+        if (index !== -1) {
+          const el2 = entry.target;
+          if (entry.isIntersecting) {
+            el2.classList.add(this._cssVisibleClass);
+            this._emit("intersect");
+            observer.disconnect();
+          }
+        }
+      });
+    }, this._options.intersectionOptions);
+    observer.observe(el);
   }
   _triggerStart() {
     if (this._validateTimeline()) {
@@ -165,6 +207,15 @@ class Movinblocks {
     this._options.overlap = overlap;
     return this;
   }
+  setViewportTrigger(intersectionOptions = null) {
+    this._options.viewportTrigger = true;
+    this._options.intersectionOptions = intersectionOptions || {
+      root: null,
+      threshold: 0,
+      rootMargin: "0px"
+    };
+    return this;
+  }
   setAnimation(animation) {
     this._options.animation = animation;
     return this;
@@ -188,11 +239,12 @@ class Movinblocks {
   }
   destroy() {
     for (const item of this._payload) {
-      item.el.classList.remove("mb");
+      item.el.classList.remove(this._cssBaseClass);
+      item.el.classList.remove(this._cssVisibleClass);
       item.el.classList.remove(item.animation);
-      Utils.removeCssVar(item.el, `${this._cssPrefix}duration`);
-      Utils.removeCssVar(item.el, `${this._cssPrefix}delay`);
-      Utils.removeCssVar(item.el, `${this._cssPrefix}timing-function`);
+      Utils.removeCssVar(item.el, `${this._cssVarPrefix}duration`);
+      Utils.removeCssVar(item.el, `${this._cssVarPrefix}delay`);
+      Utils.removeCssVar(item.el, `${this._cssVarPrefix}timing-function`);
       item.el.removeEventListener("animationstart", () => this._handleAnimationStart());
       item.el.removeEventListener("animationend", () => this._handleAnimationEnd(item.id));
       item.el.removeEventListener("animationiteration", () => this._handleAnimationIteration());
